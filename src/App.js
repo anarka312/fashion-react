bd6bf3af3cde23056c3103ff45e53770d6262b29
anarka312 <natalie.talas@gmail.com>
2023-05-25 15:01:48 +0600

    new react page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/header/header'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React!</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return (
+        <header>
+            Logo & Nav
+        </header>
+    )
+}
+
+export default Header;
